997651ca9f6e99e4ed0cc3974ba8a771e678d17b
Jan Kaluza <jkaluza@redhat.com>
2015-07-01 07:47:34 +0200

    Store httpd-cfg results in 'results' directory, cleanup apache-tests

--- tests/run.py
+++ tests/run.py
@@ -14,24 +14,27 @@ class TestImplementation:
     def pre_start(self):
         os.chdir("webconftests")
         os.system("cp -a -R ../apache-test .")
-        if not os.path.exists("./httpd.conf"):
-            os.system("cd apache-test; perl Makefile.PL; make; t/TEST -clean; t/TEST; cd ..")
+        os.system("cd apache-test; perl Makefile.PL; make; t/TEST -clean; t/TEST; cd ..")
         os.system("cp -a -R apache-test/t/conf/httpd.conf .")
         os.system("cp -a -R my-static-dir apache-test/t")
         os.system("cp -a -R 0* apache-test/t")
 
+    def stop(self):
+        os.system("rm -rf apache-test")
+        os.system("rm -rf __pycache__")
+        os.system("rm -rf httpd.conf")
+
+    def generate_cfg(self, d):
+        cmd = 'export `cat {0}`; ../../httpd-cfg {1} {2} --debug; diff -u ../results/{3}/webconf.result {4}/*.conf'.format(d + "/test.env", d, d, d, d)
+        return os.system(cmd)
+
     def start_server(self, d):
         os.system("killall " + HTTPD_BINARY + " >/dev/null 2>&1")
         os.system("sed -i 's|\"\\./|\"" + os.getcwd() + "/apache-test/t/|g' " + d + "/*.conf")
-        #print("sed -i 's|\"./|" + os.getcwd() + "/apache-test/t/|g' " + d + "/*.conf")
         os.system(HTTPD_BINARY + " -f ./httpd.conf -d ./ -c \"Include ../../" + d + "/*.conf\"")
 
     def stop_server(self, d):
         os.system("killall " + HTTPD_BINARY + " >/dev/null 2>&1")
-
-    def generate_cfg(self, d):
-        cmd = 'export `cat {0}`; ../../httpd-cfg {1} {2} --debug; diff -u {3}/webconf.result {4}/*.conf'.format(d + "/test.env", d, d, d, d)
-        return os.system(cmd)
 
 impl = TestImplementation()
 runtests(impl)
